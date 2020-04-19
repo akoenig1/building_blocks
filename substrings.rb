079d1972472ac540093f1eb8c51d473cf5518e05
Alex Koenig <alex.koenig4@gmail.com>
2020-04-19 17:04:39 -0400

    Find matching substrings from multi-word input

--- substrings.rb
+++ substrings.rb
@@ -2,17 +2,21 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings words, subs
     matches = {}
-    
+    words.downcase().split(/\W/).each { |word|
         subs.each{ |str|     
-            if words.include? str
+            if word.include? str
+                # increase count if already in hash
                 if matches[str]
                     matches[str] += 1
+                # otherwise add to hash
                 else
                     matches[str] = 1
                 end
             end
         }
+    }
     matches
 end
 
 puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
