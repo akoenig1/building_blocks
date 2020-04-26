84c5b4050ea130ee985b668786becc2e6ef16882
Alex Koenig <alex.koenig4@gmail.com>
2020-04-26 01:54:39 -0400

    Implement recursive bubble sort with swap tracker
    Created a recursive bubble sort method that takes array as an input,
    compares all adjacent values and swaps the first value with the second
    if necessary. If any values are swapped, swap tracker is flagged.
    
    After one pass through the array, the method is called recursively on
    the entire array if the swap tracker was flagged. If not, the numbers
    are already in order and the ordered array is returned.
    
    Swap tracking adds a minimal degree of efficiency. Method could be
    marginally more efficient if only n-1 values were compared in each
    recursive call of bubble_sort (because we know the highest value made
    it to n). Could not figure out how to keep track of the full, sorted
    array without adding an additional parameter to bubble_sort. Decided
    not to do this since the assignment only shows a method that accepts
    one parameter.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,23 @@
-#Accept input array
-#Compare first two values - switch if first is greater than second
-#Repeat this process with the second and third values and adjacent pairs through the rest of the array
-#Run the above process on the array again but only for array.length - 1 because we know the largest number made it to the last spot already
-#Continue running the above process through array.length = 2
-#Keep track of swaps on each pass through array. If no swaps are made in a full pass through array, we know the numbers are in order
+#Accept input array, initialize array index to 0 and assume no values will be swapped
+def bubble_sort array 
+    n = 0
+    swapped = false
+    #Compare each adjacent pair of values in the array, swap if first is greater than second
+    while n < (array.length - 1)
+        if array[n] > array [n+1]
+            array[n], array[n+1] = array[n+1], array[n]
+            #Set swap indicator to true
+            swapped = true
+        end
+        n += 1
+    end
+    #If any numbers were swapped, need to take another pass through the array
+    if swapped == true
+        array = bubble_sort(array)
+    #If no numbers were swapped, array is already in order!
+    else
+        array.join(" ")
+    end
+end
+
+puts bubble_sort([4,3,78,2,0,2])
